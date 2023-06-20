216b573492abfac742a39d9fe2fb9b7cf5e90ba7
amit <amitksingh1490@gmail.com>
2023-06-20 20:14:02 +0530

    feat: working version 1

--- Formula/cliApp.rb
+++ Formula/cliApp.rb
@@ -1,23 +1,21 @@
 class Cliapp < Formula
-    desc "Tailcall CLI App"
-    homepage "https://github.com/tailcallhq/tailcall/"
-    url "https://github.com/tailcallhq/tailcall/releases/download/v0.1.0/tailcall-v0.1.0.zip"
-    sha256 "9aa0802c0c3aa2b7fe8c8245cefb0d4daee214183e0fd5a71ec1003c9c9e09c8"
-    version "0.1.0"
-  
-    depends_on "openjdk@11"
-  
-    def install
-      libexec.install Dir["*"]
-      (bin/"tc").write <<~EOS
+  desc "Tailcall CLI App"
+  homepage "https://github.com/tailcallhq/tailcall/"
+  url "https://github.com/tailcallhq/tailcall/releases/download/v0.1.0/tailcall-v0.1.0.zip"
+  sha256 "9aa0802c0c3aa2b7fe8c8245cefb0d4daee214183e0fd5a71ec1003c9c9e09c8"
+  version "0.1.0"
+
+  depends_on "openjdk@11"
+
+  def install
+    libexec.install Dir["*"]
+    (bin/"tc").write <<~EOS
       #!/bin/sh
-      exec java -jar #{libexec}/lib/cli-assembly-0.1.0-SNAPSHOT.jar "$@"
+       #{libexec}/bin/tailcall_cli_main "$@"
     EOS
-      
-    end
-  
-    test do
-      system "#{bin}/tc"
-    end
   end
-  
+
+  test do
+    system "#{bin}/tc"
+  end
+end
