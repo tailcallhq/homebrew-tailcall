794d31088c46640970a88ef781f15907a07fd1b3
amit <amitksingh1490@gmail.com>
2023-06-20 17:19:17 +0530

    feat: add initial formula

--- Formula/tc-cli.rb
+++ Formula/tc-cli.rb
@@ -2,7 +2,7 @@ class Tailcall CLI < Formula
     desc "Tailcall CLI App"
     homepage "https://github.com/tailcallhq/tailcall/"
     url "https://github.com/tailcallhq/tailcall/releases/download/v0.1.0/tailcall-v0.1.0.zip"
-    sha256 ""
+    sha256 "9aa0802c0c3aa2b7fe8c8245cefb0d4daee214183e0fd5a71ec1003c9c9e09c8"
     version "0.1.0"
   
     depends_on "openjdk@11"
